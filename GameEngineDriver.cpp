f5c0221361e772d49b9fe40d1ab37d7a956ca8e5
SebfailedAgain <aspiring.programmer69@gmail.com>
2022-09-27 16:16:19 -0400

    Created basic file structure and file setup

---
+++ GameEngineDriver.cpp
@@ -0,0 +1,11 @@
+#include "GameEngine.h"
+using namespace std;
+#include <iostream>
+
+void testGameStates() {
+    cout << "This game has no state" << endl;
+}
+
+int main() {
+    testGameStates();
+}
