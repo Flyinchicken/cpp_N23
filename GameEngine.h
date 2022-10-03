eb034af1f18c809efcd2039ff4746e9287ab7d62
SebfailedAgain <aspiring.programmer69@gmail.com>
2022-10-03 17:13:00 -0400

    Added some various comments to header file for both mine and the viewers at homes convenience. Changed currentGameState to an int and added a setter for it. Added private function isValidStateTransition that will check if a game state transition is good. Added public function processCommandString that will process a command string from the terminal and determine what to do with it.

--- GameEngine.h
+++ GameEngine.h
@@ -1,21 +1,38 @@
-#ifndef GAMEENGINE_H
-#define GAMEENGINE_H
+#pragma once
 
-using namespace std;
 #include <string>
 #include <ostream>
 
+using namespace std;
+
+/**
+ * State = certain phase of game
+ *          What are valid actions and user commands take place
+ *          Some action may trigger transition to another state
+ * 
+ * States = 
+ *      Start, Map loaded, Map validated, Players added, Assign reinforcement, issue orders, execute orders, win
+ * Command strings =
+ *      loadmap, validatemap, addplayer, assigncounties, issueorder, endissueorders, execorder, endexecorders, win, play, end
+ * */
 class GameEngine {
     private:
-        string *currentGameState;
-        friend ostream& operator <<(ostream&, const GameEngine&);
+        int *currentGameState;                                      // What state the game is in currently
+
+        bool isValidStateTransition(int newGameState);              // Determines if can move from one to another
+
+        friend ostream& operator <<(ostream&, const GameEngine&);   // Assignment insertion operator
+
     public:
-        GameEngine();
-        GameEngine(string startingState);
-        ~GameEngine();
-        GameEngine(const GameEngine&);
+        GameEngine();                                       // Default constructor
+        ~GameEngine();                                      // Destructor
+        GameEngine(const GameEngine&);                      // Copy constructor
+        GameEngine &operator=(const GameEngine &engine);    // Assignment operator
 
-        string* getCurrentGameState();
+        int getCurrentGameState();
+        int setCurrentGameState(int newGameState);
+        // Would need constant string array/dictionary of valid command strings to compare against
+        // If valid, try to set current
+        // If not valid, complain
+        void processCommandString(string commandString);
 };
-
-#endif
