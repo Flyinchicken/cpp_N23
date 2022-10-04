992947f431d740e5365adb269e194b0452a88cca
KubrayKan <guillermo.tremols@gmail.com>
2022-10-04 01:35:37 -0400

    DUB

--- Card.cpp
+++ Card.cpp
@@ -1,1 +1,111 @@
-//cpp file
+#pragma once
+#include<iostream>
+#include "Card.h"
+#include<string>
+using namespace std;
+
+//Card definitions
+
+Card::Card(){
+    cout << "card Created" <<endl;
+};
+
+Card::Card(string* description, string* type, string* name): _name(name), _description(description), _type(type){
+    cout <<"card created" <<endl;
+};
+
+Card::Card(const Card& e){
+    this->_description = new string(*(e._description)); 
+    this->_type = new string(*(e._type)); 
+    this->_name = new string(*(e._name)); 
+};
+
+Card& Card::operator =(const Card& e){
+    this->_description = new string(*(e._description)); 
+    this->_type = new string(*(e._type)); 
+    this->_name = new string(*(e._name)); 
+    return *this;
+};
+
+ostream& operator<<(std::ostream& stream, Card& e){
+    stream << "This is Card " << e.getName() << " of type " << e.getType() << ".\n Description: " << e.getDescription() << endl;
+    return stream;
+};
+
+Card::~Card(){
+    
+};
+
+void Card::play() {
+    cout << "Playing" << endl;
+};
+
+string Card::getName() {
+    return *_name;
+};
+
+string Card::getDescription() {
+    return *_description;
+};
+
+string Card::getType(){
+    return *_type;
+};
+
+//Deck Definitions
+Deck::Deck() {
+    cout << "Deck Created" << endl;
+};
+
+Deck::Deck(vector<Card*> input) : _cardDeck(input){
+    cout << "Deck created" << endl;
+};
+
+Deck::Deck(const Deck& e) {
+    this->_cardDeck = e._cardDeck;
+};
+
+Deck& Deck::operator =(const Deck& e) {
+    this->_cardDeck = e._cardDeck;
+    return *this;
+};
+
+ostream& operator<<(std::ostream& stream, Deck& e) {
+    stream << "This is the Deck and it has "<<e._cardDeck.size() << endl;
+    return stream;
+};
+
+Deck::~Deck() {
+
+};
+
+void Deck::draw() {
+    cout << "I draw" << endl ;
+};
+
+//Hand Definitions
+Hand::Hand() {
+    cout << "Hand Created" << endl;
+};
+
+Hand::Hand(vector<Card*> input) : _cardHand(input) {
+    cout << "Hand created" << endl;
+};
+
+Hand::Hand(const Hand& e) {
+    this->_cardHand = e._cardHand;
+};
+
+Hand& Hand::operator =(const Hand& e) {
+    this->_cardHand = e._cardHand;
+    return *this;
+};
+
+ostream& operator<<(std::ostream& stream, Hand& e) {
+    stream << "This is the Hand and it has " << e._cardHand.size() << endl;
+    return stream;
+};
+
+Hand::~Hand() {
+
+};
--- Card.h
+++ Card.h
@@ -1,2 +1,60 @@
-//header file
-//gaming
+#pragma once
+#include<string>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Card
+
+{
+private:
+    string* _description;
+    string* _type;
+    string* _name;
+public:
+    Card();
+    Card(string* description, string* type, string* name);
+    Card(const Card& e);
+    Card& operator =(const Card& e);
+    friend ostream& operator<<(std::ostream& stream, Card& e);
+    ~Card();
+
+    string getDescription();
+    string getType();
+    string getName();
+
+    void play();
+};
+
+class Deck
+{
+private:
+    vector<Card*> _cardDeck;
+public:
+    Deck();
+    Deck(vector<Card*> e);
+    Deck(const Deck& e);
+    Deck& operator =(const Deck& e);
+    friend ostream& operator<<(std::ostream& stream, Deck& e);
+    ~Deck();
+
+    void draw();
+};
+
+class Hand
+{
+private:
+    vector<Card*> _cardHand;
+public:
+    Hand();
+    Hand(vector<Card*> e);
+    Hand(const Hand& e);
+    Hand& operator =(const Hand& e);
+    friend ostream& operator<<(std::ostream& stream, Hand& e);
+    ~Hand();
+};
+
+
+
+
+
