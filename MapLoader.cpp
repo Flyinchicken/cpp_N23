5d8ad625e48a30015857141d39dd23b667e0f25f
broxzhang <broxzhang@gmail.com>
2022-10-05 10:59:24 -0400

    add maploader class

--- MapLoader.cpp
+++ MapLoader.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "MapLoader.h"
-
+#include "Map.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -37,11 +37,57 @@ void MapLoader::LoadMap() {
         cout << mapFiles[i].replace(0,12, "") << endl;
     }
 
+//    cout << "Please enter the map file name: " << endl;
+
+    ifstream input("../MapFiles/3D.map");
+
+    if(!input.is_open()) {
+        cout << "File not found!" << endl;
+        return;
+    }
+
+    vector<Continent>* continents = new vector<Continent>();
+    vector<Territory>* territories = new vector<Territory>();
+
+    bool readingContinent = false;
+    bool readingTerritory = false;
+
+    while( !input.eof() ) {
+        string line;
+        getline(input, line);
+        if (line == "[Continents]\r") {
+            readingContinent = true;
+            continue;
+        }
+        if (line == "[Territories]\r") {
+            readingContinent = false;
+            readingTerritory = true;
+            continue;
+        }
+
+        if(readingContinent) {
+            Continent continent = Continent();
+            continent.name = line.substr(0, line.find('='));
+            continent.value = stoi(line.substr(line.find('=') + 1));
+            continents->push_back(continent);
+        }
+
+        if(readingTerritory) {
+            vector<string> territoryInfo;
+
+            stringstream ss(line);
+
+            while (ss.good()) {
+                string substr;
+                getline(ss, substr, ',');
+                territoryInfo.push_back(substr);
+            }
+        }
+    }
 
 //    ifstream input("001_I72_Ghtroc720.map");
 //
-//    vector<Continent>* continents = new vector<Continent>();
-//    vector<Territory>* territories = new vector<Territory>();
+
 //
 //
 //
@@ -85,6 +131,6 @@ void MapLoader::LoadMap() {
 //    }
 //
 //
-//    input.close();
+    input.close();
 
 }
