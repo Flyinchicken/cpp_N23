245e92b4dcd56c75ba60ea3c92a152f752bb6748
KubrayKan <guillermo.tremols@gmail.com>
2022-11-12 22:52:45 -0500

    dsadewq

--- Main.cpp
+++ Main.cpp
@@ -19,6 +19,8 @@ int main(int argc, char** argv)
     
     vector<int> numbers1 = numbers;
     reverse(numbers1.begin(), numbers1.end());
+
+    string temp;
     
     vector<int>::iterator it = find(numbers.begin(), numbers.end(), 20); //Tries to find the Card inside the card by assigning an itetator to the index
     vector<int>::iterator it1 = find(numbers1.begin(), numbers1.end(), 20); //Tries to find the Card inside the card by assigning an itetator to the index
@@ -26,6 +28,8 @@ int main(int argc, char** argv)
     if (it != numbers.end()) { //If the card is found
         cout << it - numbers.begin() << endl;
     }
+
+    cin >> temp;
     
     if (it1 != numbers1.end()) { //If the card is found
         cout << numbers.size() - 1 - (it1 - numbers1.begin()) << endl;
