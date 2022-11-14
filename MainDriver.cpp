dfa20953796c841917fc1666d84a604bcbdc2bd3
SebfailedAgain <aspiring.programmer69@gmail.com>
2022-11-13 23:16:39 -0500

    MainDriver calls all five testing functions now

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -20,41 +20,16 @@ Deck *x = new Deck();
 
 int main(int argc, char **argv)
 {
-
-    //testOrderExecution();
     testCommandProcessor(argc, argv);
 
     // Need file path before creating
-    // ge = new GameEngine();
+    ge = new GameEngine();
 
-    // testLoggingObserver();
+    testStartupPhase();
 
-    // testStartupPhase();
+    testMainGameLoop();
 
-    // testCommandProcessor();
+    testOrderExecution();
 
-    /*std::cout << "Welcome everyone!" << std::endl;
-
-    // Map Test
-    cout << "*** First, demonstrate map load test ***" << endl;
-    //testLoadMaps();
-
-    // Game Engine Tests
-    std::cout << std::endl
-              << "Game Engine Tests Commencing" << std::endl
-              << std::endl;
-
-    //testGameStates();
-
-    // OrdersList Test
-    cout << "*** Order list test ***" << endl;
-    testOrdersLists();
-
-    // Card Test
-    cout << "*** Starting the Card test ***" << endl;
-    testCards();
-
-    // Player Test
-    cout << "*** Starting the Player test ***" << endl;
-    testPlayers();*/
+    testLoggingObserver();
 }
