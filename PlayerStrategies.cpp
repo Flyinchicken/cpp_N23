b3fbd37f7dc85907a4cc77f289c8380b6ebc7803
SebfailedAgain <aspiring.programmer69@gmail.com>
2022-11-22 16:12:01 -0500

    Removed include of header files in header file and added forward class declarations instead. Added Player to PlayerStrategy along with constructor and destructor for each strategy. Neutral player is techincally implemented now, and started on Cheater (not relly much)

--- PlayerStrategies.cpp
+++ PlayerStrategies.cpp
@@ -1,4 +1,5 @@
 #include "PlayerStrategies.h"
+#include "Map.h"
 
 ostream& operator << (ostream &out, const PlayerStrategy &strategy) {
     out << strategy.getStrategyAsString();
@@ -6,9 +7,22 @@ ostream& operator << (ostream &out, const PlayerStrategy &strategy) {
     return out;
 }
 
+PlayerStrategy::PlayerStrategy(Player *p) {
+    this->player = p;
+}
+
+PlayerStrategy::~PlayerStrategy() {
+    delete this->player;
+}
+
 ///
 /// HUMAN PLAYER
 ///
+
+HumanPlayerStrategy::HumanPlayerStrategy(Player *p) : PlayerStrategy(p) { }
+
+HumanPlayerStrategy::~HumanPlayerStrategy() {
+}
 
 void HumanPlayerStrategy::issueOrder() {
 
@@ -26,9 +40,20 @@ string HumanPlayerStrategy::getStrategyAsString() const {
     return "Human Player";
 }
 
+ostream& operator << (ostream& out, const HumanPlayerStrategy &strategy) {
+    out << strategy.getStrategyAsString();
+
+    return out;
+}
+
 /// 
 /// AGGRESSIVE PLAYER
 ///
+
+AggressivePlayerStrategy::AggressivePlayerStrategy(Player *p) : PlayerStrategy(p) { }
+
+AggressivePlayerStrategy::~AggressivePlayerStrategy() {
+}
 
 void AggressivePlayerStrategy::issueOrder() {
 
@@ -46,9 +71,20 @@ string AggressivePlayerStrategy::getStrategyAsString() const {
     return "Aggressive Player (bot)";
 }
 
+ostream& operator << (ostream& out, const AggressivePlayerStrategy &strategy) {
+    out << strategy.getStrategyAsString();
+
+    return out;
+}
+
 ///
 /// BENEVOLENT PLAYER
 ///
+
+BenevolentPlayerStrategy::BenevolentPlayerStrategy(Player *p) : PlayerStrategy(p) { }
+
+BenevolentPlayerStrategy::~BenevolentPlayerStrategy() {
+}
 
 void BenevolentPlayerStrategy::issueOrder() {
 
@@ -66,29 +102,60 @@ string BenevolentPlayerStrategy::getStrategyAsString() const {
     return "Benevolent Player (bot)";
 }
 
+ostream& operator << (ostream& out, const BenevolentPlayerStrategy &strategy) {
+    out << strategy.getStrategyAsString();
+
+    return out;
+}
+
 ///
 /// NEUTRAL PLAYER
 ///
 
+NeutralPlayerStrategy::NeutralPlayerStrategy(Player *p) : PlayerStrategy(p) { }
+
+NeutralPlayerStrategy::~NeutralPlayerStrategy() {
+}
+
+/**
+ * Doesn't issue any orders
+*/
 void NeutralPlayerStrategy::issueOrder() {
 
 }
 
+/**
+ * @returns Empty vector since will not attack anything
+*/
 vector<Territory*> NeutralPlayerStrategy::toAttack() {
-
+    return {};
 }
 
+/**
+ * @returns Empty vector since will not attack anything
+*/
 vector<Territory*> NeutralPlayerStrategy::toDefend() {
-
+    return {};
 }
 
 string NeutralPlayerStrategy::getStrategyAsString() const {
     return "Neutral Player (bot)";
 }
 
+ostream& operator << (ostream& out, const NeutralPlayerStrategy &strategy) {
+    out << strategy.getStrategyAsString();
+
+    return out;
+}
+
 ///
 /// CHEATER PLAYER
 ///
+
+CheaterPlayerStrategy::CheaterPlayerStrategy(Player *p) : PlayerStrategy(p) { }
+
+CheaterPlayerStrategy::~CheaterPlayerStrategy() {
+}
 
 void CheaterPlayerStrategy::issueOrder() {
 
@@ -98,10 +165,20 @@ vector<Territory*> CheaterPlayerStrategy::toAttack() {
 
 }
 
+vector<Territory*> CheaterPlayerStrategy::getAdjacentEnemyTerritories() {
+    
+}
+
 vector<Territory*> CheaterPlayerStrategy::toDefend() {
 
 }
 
 string CheaterPlayerStrategy::getStrategyAsString() const {
     return "Cheater Player (bot)";
+}
+
+ostream& operator << (ostream& out, const CheaterPlayerStrategy &strategy) {
+    out << strategy.getStrategyAsString();
+
+    return out;
 }
--- PlayerStrategies.h
+++ PlayerStrategies.h
@@ -1,26 +1,36 @@
 #pragma once
 
 #include <vector>
-
-#include "Map.h"
+#include <iostream>
+#include <string>
 
 using std::vector;
+using std::ostream;
+using std::string;
 
 class Territory;
+class Player;
 
 // Abstract strategy class
 class PlayerStrategy {
     friend ostream& operator << (ostream&, const PlayerStrategy&);
     public:
+        virtual ~PlayerStrategy();
         virtual void issueOrder() = 0;
         virtual vector<Territory*> toAttack() = 0;
         virtual vector<Territory*> toDefend() = 0;
         virtual string getStrategyAsString() const = 0;
+    protected:
+        PlayerStrategy(Player*);
+        Player *player;
 };
 
 // Concrete strategy classes
 class HumanPlayerStrategy : public PlayerStrategy {
+    friend ostream& operator << (ostream&, const HumanPlayerStrategy&);
     public:
+        HumanPlayerStrategy(Player*);
+        ~HumanPlayerStrategy();
         void issueOrder();
         vector<Territory*> toAttack();
         vector<Territory*> toDefend();
@@ -28,7 +38,10 @@ class HumanPlayerStrategy : public PlayerStrategy {
 };
 
 class AggressivePlayerStrategy : public PlayerStrategy {
+    friend ostream& operator << (ostream&, const AggressivePlayerStrategy&);
     public:
+        AggressivePlayerStrategy(Player*);
+        ~AggressivePlayerStrategy();
         void issueOrder();
         vector<Territory*> toAttack();
         vector<Territory*> toDefend();
@@ -36,7 +49,10 @@ class AggressivePlayerStrategy : public PlayerStrategy {
 };
 
 class BenevolentPlayerStrategy : public PlayerStrategy {
+    friend ostream& operator << (ostream&, const BenevolentPlayerStrategy&);
     public:
+        BenevolentPlayerStrategy(Player*);
+        ~BenevolentPlayerStrategy();
         void issueOrder();
         vector<Territory*> toAttack();
         vector<Territory*> toDefend();
@@ -44,7 +60,10 @@ class BenevolentPlayerStrategy : public PlayerStrategy {
 };
 
 class NeutralPlayerStrategy : public PlayerStrategy {
+    friend ostream& operator << (ostream&, const NeutralPlayerStrategy&);
     public:
+        NeutralPlayerStrategy(Player*);
+        ~NeutralPlayerStrategy();
         void issueOrder();
         vector<Territory*> toAttack();
         vector<Territory*> toDefend();
@@ -53,9 +72,14 @@ class NeutralPlayerStrategy : public PlayerStrategy {
 
 // ??Not in instructions??
 class CheaterPlayerStrategy : public PlayerStrategy {
+    friend ostream& operator << (ostream&, const CheaterPlayerStrategy&);
     public:
+        CheaterPlayerStrategy(Player*);
+        ~CheaterPlayerStrategy();
         void issueOrder();
         vector<Territory*> toAttack();
         vector<Territory*> toDefend();
         string getStrategyAsString() const;
+    private:
+        vector<Territory*> getAdjacentEnemyTerritories();
 };
