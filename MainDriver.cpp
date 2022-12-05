c4d4c7b8e792c5b7f85c9854e1e2a109fda2ed3b
SebfailedAgain <aspiring.programmer69@gmail.com>
2022-12-04 19:14:21 -0500

    Made MainDriver official, so to say

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -23,48 +23,9 @@ bool realGame;
 int main(int argc, char** argv)
 {
     x = new Deck();
+    ge = new GameEngine();
     // This is a test since a test is being run
     realGame = false;
     testPlayerStrategies();
-    //testOrderExecution();
-
-    // Need file path before creating
-    // ge = new GameEngine();
-    // ge = new GameEngine();
-    // x = new Deck();
-    // testLoggingObserver();
-    // vector<Territory*> neighbors = worldMap->getNeighboursPtr();
-    //testStartupPhase();
-
-    //testMainGameLoop();
-
-    //testStartupPhase();
-    //testMainGameLoop();
-    // testCommandProcessor();
-    //testOrderExecution();
-    /*std::cout << "Welcome everyone!" << std::endl;
-
-    // Map Test
-    cout << "*** First, demonstrate map load test ***" << endl;
-    //testLoadMaps();
-
-    // Game Engine Tests
-    std::cout << std::endl
-              << "Game Engine Tests Commencing" << std::endl
-              << std::endl;
-
-    //testGameStates();
-
-    // OrdersList Test
-    cout << "*** Order list test ***" << endl;
-    testOrdersLists();
-
-    // Card Test
-    cout << "*** Starting the Card test ***" << endl;
-    testCards();
-
-    // Player Test
-    cout << "*** Starting the Player test ***" << endl;
-    testPlayers();*/
-    //}
+    testTournament();
 }
